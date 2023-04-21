39c849c2323da99066cb4dd1f04468f51db75138
KawaguchiNozomi <k022g1004@g.neec.ac.jp>
2023-04-21 13:53:59 +0900

    モデルの描画

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -4,7 +4,7 @@
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() { delete sprite_; }
+GameScene::~GameScene() { delete model_; }
 
 void GameScene::Initialize() {
 
@@ -12,20 +12,16 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 
-	//ファイル名を指定してテクスチャを読み込む
 	textureHandle_ = TextureManager::Load("sample.png");
-	//スプライトの生成
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	model_ = Model::Create();
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
 }
 
 void GameScene::Update() { 
-	//スプライトの今の座標を取得
-	Vector2 position = sprite_->GetPosition(); 
-	//座標を{2,1}移動
-	position.x += 2.0f;
-	position.y += 1.0f;
-	//移動した座標をスプライトに反映
-	sprite_->SetPosition(position);
+
 }
 
 void GameScene::Draw() {
@@ -54,6 +50,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -66,7 +63,6 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	sprite_->Draw();
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
